2ca269e6aefea843dcea48db406f75da63876f08
sh0kh <shoxruuh07tuxtanazarov@gmail.com>
2025-02-07 09:30:37 +0500

    edut

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,4 +1,4 @@
 import axios from 'axios'
-axios.defaults.baseURL = 'http://185.196.213.159:7080/savol/xona/api'
+axios.defaults.baseURL = 'http://194.87.151.210:7080/savol/xona/api'
 
 export default axios
